69880c9539156cb653d45a55f716c7fbc7479168
45ninjas <tom.parker1235@gmail.com>
2022-06-13 15:51:02 +0800

    Changed the way the dynamic mesh works
    Signed-off-by: 45ninjas <tom.parker1235@gmail.com>

--- src/proc/dynamic_mesh.rs
+++ src/proc/dynamic_mesh.rs
@@ -1,5 +1,5 @@
 use bevy::{prelude::*, render::mesh::Indices};
-use bevy_rapier3d::{prelude::*, rapier::prelude::ColliderBuilder};
+use bevy_rapier3d::prelude::*;
 
 pub struct DynamicMeshPlugin;
 impl Plugin for DynamicMeshPlugin {
@@ -12,8 +12,7 @@ impl Plugin for DynamicMeshPlugin {
 /// Contains the vertices, uvs and triangles used for building meshes.
 #[derive(Component, Default)]
 pub struct DynamicMesh {
-    verts: Vec<Vertex>,
-    triangles: Vec<[u32; 3]>,
+    triangles: Vec<[Vertex; 3]>,
 }
 
 impl DynamicMesh {
@@ -24,10 +23,10 @@ impl DynamicMesh {
     }
     /// Clears the mesh maker.
     pub fn clear(&mut self) {
-        self.verts.clear();
         self.triangles.clear();
     }
 
+    /// Gets the total triangle count.
     pub fn tri_count(&self) -> usize {
         self.triangles.len()
     }
@@ -38,48 +37,66 @@ impl DynamicMesh {
             .cross(positions[2] - positions[0])
             .normalize();
 
-        let mut tri: [u32; 3] = [0; 3];
+        let mut tri: [Vertex; 3] = [Vertex { ..default() }; 3];
 
         for i in 0..3 {
-            let vertex = Vertex {
+            tri[i] = Vertex {
                 normal: normal,
                 position: positions[i],
+                uv: Vec2::ZERO,
             };
-
-            tri[i] = self.insert(vertex);
         }
         self.triangles.push(tri);
     }
 
-    /// Inserts a vertex and returns it's index.
-    pub fn insert(&mut self, vertex: Vertex) -> u32 {
-        let index = self.verts.iter().position(|&x| x == vertex);
-        match index {
-            Some(index) => index as u32,
-            None => {
-                self.verts.push(vertex);
-                self.verts.len() as u32 - 1
+    /// Reduce the vertices and triangles with different rules.
+    pub fn distil(&self, rule: &CompareRule) -> (Vec<[u32; 3]>, Vec<Vertex>) {
+
+        // Our new 'mesh'
+        let mut triangles = Vec::new();
+        let mut vertices: Vec<Vertex> = Vec::new();
+
+        // Go over each vertex in each triangle
+        for triangle in self.triangles.iter() {
+            let mut indices = [0; 3];
+            for i in 0..3 {
+
+                // Get the index of an existing or similar vertex based on our rule.
+                let index = vertices.iter().position(|&x| triangle[i].compare(&x, rule));
+
+                // Set the index of the current vertex to the existing or new index.
+                indices[i] = match index {
+                    Some(index) => index as u32,
+                    None => {
+                        vertices.push(triangle[i]);
+                        vertices.len() as u32 - 1
+                    }
+                };
             }
+            triangles.push(indices);
         }
+
+        (triangles, vertices)
     }
 
     /// Update an existing mesh.
     pub fn update_mesh(&self, mesh: &mut Mesh) {
+        let (tris, verts) = self.distil(&CompareRule::Mesh);
         // Migrate our Vec3 position and normal data to [f32; 3] and generate our uvs.
         // TODO: learn how to do this more elegantly like how indices/triangles works.
         let mut vertices = Vec::new();
         let mut normals = Vec::new();
         let mut uvs = Vec::new();
-        for v in self.verts.iter() {
+        for v in verts.iter() {
             vertices.push([v.position.x, v.position.y, v.position.z]);
             normals.push([v.normal.x, v.normal.y, v.normal.z]);
-            // normals.push([0.0, 0.0, 1.0]);
-            uvs.push([v.position.x, v.position.z]);
+            // uvs.push([v.position.x, v.position.z]);
+            uvs.push([v.uv.x, v.uv.y]);
         }
 
         let mut triangles = Vec::new();
 
-        for tri in self.triangles.iter() {
+        for tri in tris.iter() {
             triangles.push(tri[0]);
             triangles.push(tri[1]);
             triangles.push(tri[2]);
@@ -91,19 +108,30 @@ impl DynamicMesh {
         // Set our triangles.
         let indices = Indices::U32(triangles);
         mesh.set_indices(Some(indices));
+
+        println!(
+            "Updated a mesh with {:?} tris and {:?} verts.",
+            tris.len(),
+            verts.len()
+        );
     }
 
     pub fn collider(&self) -> Collider {
+        let (tris, verts) = self.distil(&CompareRule::Collider);
         let mut vertices = Vec::new();
-        for v in self.verts.iter() {
+        for v in verts.iter() {
             vertices.push(v.position);
         }
-        let triangles = self.triangles.clone();
-        Collider::trimesh(vertices, triangles)
+        println!(
+            "Updated a collider with {:?} tris and {:?} verts.",
+            tris.len(),
+            vertices.len()
+        );
+        Collider::trimesh(vertices, tris)
     }
 }
 
-// Updates a Mesh when the DynamicMesh has been changed.
+//// Updates a Mesh when the DynamicMesh has been changed.
 fn mesh_on_changed(
     mut assets: ResMut<Assets<Mesh>>,
     query: Query<(&Handle<Mesh>, &DynamicMesh), Changed<DynamicMesh>>,
@@ -115,6 +143,7 @@ fn mesh_on_changed(
     }
 }
 
+/// Replaces an existing collider with one derived from our changed DynamicMesh.
 fn collider_on_changed(
     mut commands: Commands,
     query: Query<(Entity, &DynamicMesh, &Collider), Changed<DynamicMesh>>,
@@ -129,10 +158,32 @@ fn collider_on_changed(
     }
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Default)]
 pub struct Vertex {
     pub position: Vec3,
     pub normal: Vec3,
+    pub uv: Vec2,
+}
+
+pub enum CompareRule {
+    Mesh,
+    MeshNoUV,
+    Collider,
+}
+
+impl Vertex {
+    fn compare(&self, other: &Vertex, rule: &CompareRule) -> bool {
+        const MAX_ABS_DIFF: f32 = 0.01;
+
+        match rule {
+            CompareRule::Mesh => self.eq(other),
+            CompareRule::Collider => self.position.abs_diff_eq(other.position, MAX_ABS_DIFF),
+            CompareRule::MeshNoUV => {
+                self.position.abs_diff_eq(other.position, MAX_ABS_DIFF)
+                    && self.normal.abs_diff_eq(other.normal, MAX_ABS_DIFF)
+            }
+        }
+    }
 }
 
 impl PartialEq for Vertex {
@@ -142,5 +193,6 @@ impl PartialEq for Vertex {
 
         self.position.abs_diff_eq(other.position, MAX_ABS_DIFF)
             && self.normal.abs_diff_eq(other.normal, MAX_ABS_DIFF)
+            && self.uv.abs_diff_eq(other.uv, MAX_ABS_DIFF)
     }
 }
