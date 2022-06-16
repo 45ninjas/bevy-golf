3c1adbd648c4a60a10433dbc53957b0d037dccc6
45ninjas <tom.parker1235@gmail.com>
2022-06-16 16:07:18 +0800

    Got smoothing to work on the camera
    Signed-off-by: 45ninjas <tom.parker1235@gmail.com>

--- src/camera.rs
+++ src/camera.rs
@@ -17,10 +17,11 @@ impl Plugin for CameraPlugin {
 #[derive(Component)]
 pub struct CameraTarget;
 
-#[derive(Component)]
-pub struct Smoother {
-    smoothness: f32,
-    enabled: bool,
+#[derive(Component, Debug)]
+pub struct Smoother<T> {
+    pub smoothness: f32,
+    pub enabled: bool,
+    pub last_value: T,
 }
 
 const CAMERA_OFFSET: Vec3 = const_vec3!([-10.0, 10.0, -10.0]);
@@ -30,55 +31,54 @@ fn add_camera(mut commands: Commands) {
     camera.orthographic_projection.scale = 1.5;
     camera.transform = Transform::from_translation(CAMERA_OFFSET).looking_at(Vec3::ZERO, Vec3::Y);
 
-    commands.spawn_bundle(camera);
+    commands.spawn_bundle(camera).insert(Smoother {
+        smoothness: 4.0,
+        enabled: true,
+        last_value: CAMERA_OFFSET,
+    });
 }
 
 fn camera_follow(
     mut transforms: ParamSet<(
-        Query<(&mut Transform, Option<&mut Smoother>), With<Camera3d>>,
+        Query<(&mut Transform, Option<&mut Smoother<Vec3>>), With<Camera3d>>,
         Query<&Transform, With<CameraTarget>>,
     )>,
     time: Res<Time>,
 ) {
     // Get the middle of all the camera targets.
-    let targets_query = transforms.p1();
     let mut middle_target = Vec3::ZERO;
-    for target_transform in targets_query.iter() {
+    for target_transform in transforms.p1().iter() {
         middle_target += target_transform.translation;
     }
-    middle_target /= targets_query.iter().count() as f32;
+    middle_target /= transforms.p1().iter().count() as f32;
 
-    // Update our camera's position.
-    let mut camera_query = transforms.p0();
-    let (mut camera_transform, mut smoother) = camera_query
-        .get_single_mut()
-        .expect("A singular Camera3d doesn't exist.");
+    for (mut transform, smoother) in transforms.p0().iter_mut() {
+        let target = middle_target + CAMERA_OFFSET;
 
-    let target = middle_target + CAMERA_OFFSET;
-
-    // TODO: Add smoothing.
-    match smoother {
-        Some(_) => todo!(),
-        None => camera_transform.translation = target,
+        // TODO: Add smoothing.
+        match smoother {
+            Some(mut smoother) => {
+                transform.translation = smoother.last_value.lerp(target, smoother.smoothness * time.delta_seconds());
+                smoother.last_value = transform.translation;
+            }
+            None => transform.translation = target,
+        }
     }
 }
 
 /// Gets the cursor's position in normalised coordinates (-1, 1)
 pub fn cursor_normalised(window: &Window, origin: &WindowOrigin) -> Option<Vec2> {
-    match window.cursor_position() {
-        Some(position) => Some(match origin {
-            WindowOrigin::Center => {
-                (position / Vec2::new(window.width(), window.height()) - Vec2::new(0.5, 0.5))
+    window.cursor_position().map(|position| match origin {
+        WindowOrigin::Center => {
+            (position / Vec2::new(window.width(), window.height()) - Vec2::new(0.5, 0.5))
                 // -1.0 to 1.0
                 * Vec2::new(2.0, 2.0)
                 // Aspect ratio correction.
                 * Vec2::new(window.width() / window.height(), 1.0)
-            }
-            // TODO: Support 0,0 at the bottom left.
-            WindowOrigin::BottomLeft => todo!(),
-        }),
-        None => None,
-    }
+        }
+        // TODO: Support 0,0 at the bottom left.
+        WindowOrigin::BottomLeft => todo!(),
+    })
 }
 
 /// Convert the cursor's screen position to a camera ray for orthographic projection.
@@ -88,11 +88,7 @@ pub fn cursor_to_world_orthographic(
     window: &Window,
 ) -> Option<Ray> {
     // Get our cursor's position or return None if no cursor exists.
-    let cursor = cursor_normalised(window, &camera_projection.window_origin);
-    if cursor.is_none() {
-        return None;
-    }
-    let cursor_pos = cursor.unwrap();
+    let cursor_pos = cursor_normalised(window, &camera_projection.window_origin)?;
 
     let mut position = camera_world.right() * cursor_pos.x * camera_projection.scale;
     position += camera_world.up() * cursor_pos.y * camera_projection.scale;
@@ -103,7 +99,7 @@ pub fn cursor_to_world_orthographic(
     })
 }
 
-/// The ray 
+/// The ray
 pub struct Ray {
     pub origin: Vec3,
     pub direction: Vec3,
